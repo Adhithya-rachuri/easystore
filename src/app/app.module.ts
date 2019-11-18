bee278b252f7ed27c587917409ded819bb983824
Adhithya Reddy Rachuri <St-201906397@mcit.local>
2019-11-18 18:09:15 -0500

    adding few components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const appRoutes: Routes = [
+  { path : 'login', component : LoginComponent },
+  { path : 'registration', component : RegistrationComponent },
+  { path : 'home', component : HomeComponent }
+
+  
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent,
+    RegistrationComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  myEmail : string = '';
+  fColor : string = "#ff0000";
+  isOK = false;
+  fSize = 23;
+  
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  checkEmail(event: Event){
+    console.log(event);
+    this.myEmail = (<HTMLInputElement> event.target).value;
+  }
+
+  loginUser(){
+    //Some code to check about the credentials if are correct or not ....
+
+    this.router.navigate(['/home']);
+  }
+
+}
